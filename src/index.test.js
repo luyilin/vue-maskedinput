7641fcd668c4507b0ac950d66e02fe804b736612
luyilin <luyilin12@gmail.com>
2017-10-25 17:52:48 +0800

    publish

--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-import { mount } from 'vue-test-utils'
-import MaskedInput from './'
-
-test('it works', () => {
-  const wrapper = mount(MaskedInput)
-  expect(wrapper.isVueInstance()).toBe(true)
-})
